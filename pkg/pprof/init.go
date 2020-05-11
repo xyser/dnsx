76890c4da19c6689ac40823a9fcf25f9f395aa04
dingdayu <614422099@qq.com>
2020-05-11 21:08:12 +0800

    add pkg/pprof
    Signed-off-by: dingdayu <614422099@qq.com>

---
+++ pkg/pprof/init.go
@@ -0,0 +1,68 @@
+package pprof
+
+import (
+	"context"
+	"net/http"
+	_ "net/http/pprof"
+	"sync"
+
+	"dnsx/pkg/config"
+	"dnsx/pkg/log"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+var mu sync.Mutex
+var ctx context.Context
+var server *http.Server
+var isOpen bool
+
+const LogNamed = "pprof"
+
+// Init init server
+func Init() {
+	config.RegisterChangeEvent(func(e fsnotify.Event) {
+		ReloadConfig()
+	})
+}
+
+func ReloadConfig() {
+	// 配置未开启状态(false)
+	if !viper.GetBool("pprof.open") {
+		// 服务已开启,执行关闭
+		if getStatus() {
+			setStatus(false)
+			go server.Shutdown(ctx)
+		}
+		return
+	}
+
+	// 配置开启状态(true)且服务未开启
+	if !getStatus() {
+		ctx = context.TODO()
+		server = &http.Server{
+			Addr: viper.GetString("pprof.addr"),
+		}
+		setStatus(true)
+
+		go server.ListenAndServe()
+		log.New().Named(LogNamed).Info("start pprof", zap.String("url", viper.GetString("pprof.addr")))
+		return
+	}
+}
+
+// setStatus 设置 pprof 是否开启
+func setStatus(status bool) {
+	mu.Lock()
+	defer mu.Unlock()
+	isOpen = status
+}
+
+// getStatus 获取 pprof 是否开启
+func getStatus() bool {
+	mu.Lock()
+	defer mu.Unlock()
+	return isOpen
+}
