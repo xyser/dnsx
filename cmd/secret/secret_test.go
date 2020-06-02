f8111577b432c7f5d944e912b94b0883c5bd58b9
dingdayu <614422099@qq.com>
2020-06-02 17:52:28 +0800

    Update ignore a Test exception
    Signed-off-by: dingdayu <614422099@qq.com>

--- cmd/secret/secret_test.go
+++ cmd/secret/secret_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/miekg/dns"
 )
 
-func TestVerteiltesysteme_dot_net_rrsig(t *testing.T) {
+func TestVerificationCloudflareDotComDNSKEYRRSIG(t *testing.T) {
 	key257, _ := dns.NewRR("cloudflare.com.\t\t3599\tIN\tDNSKEY\t257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+ KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==")
 	key256, _ := dns.NewRR("cloudflare.com.\t\t3599\tIN\tDNSKEY\t256 3 13 oJMRESz5E4gYzS/q6XDrvU1qMPYIjCWzJaOau8XNEZeqCYKD5ar0IRd8 KqXXFJkqmVfRvMGPmM1x8fGAa2XhSA==")
 	sigs, _ := dns.NewRR("cloudflare.com.\t\t3599\tIN\tRRSIG\tDNSKEY 13 2 3600 20200623071422 20200424071422 2371 cloudflare.com. M4Gm5gntEncnqRht2ALaUd8tu/NHqIWTCIlu/anbncHYYE3qJx00sVTF dkuxpaRRQRRI7HXd/dJjxZi/2FWLJg==")
@@ -19,12 +19,13 @@ func TestVerteiltesysteme_dot_net_rrsig(t *testing.T) {
 	t.Log(rsk)
 	t.Log(sin)
 	t.Log(zsk)
+
 	if e := sin.Verify(zsk, []dns.RR{rsk}); e != nil {
 		//t.Errorf("cannot verify RRSIG with keytag [%d]. Cause [%s]", rsk.KeyTag(), e.Error())
 	}
 }
 
-func TestAAA(t *testing.T) {
+func TestVerificationCloudflareDotComARRSIG(t *testing.T) {
 	//key257, _ := dns.NewRR("cloudflare.com.\t\t3599\tIN\tDNSKEY\t257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+ KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==")
 	key256, _ := dns.NewRR("cloudflare.com.\t\t3599\tIN\tDNSKEY\t256 3 13 oJMRESz5E4gYzS/q6XDrvU1qMPYIjCWzJaOau8XNEZeqCYKD5ar0IRd8 KqXXFJkqmVfRvMGPmM1x8fGAa2XhSA==")
 	sigs, _ := dns.NewRR("cloudflare.com.\t\t108\tIN\tRRSIG\tA 13 2 300 20200602115010 20200531095010 34505 cloudflare.com. 1ntvMbesPT/IeNKAzB+L2UZ0OCvAqbszfQeu4Ijn0rQJVgZfs4fdKEvu Gv6a0fZxrMbAeU93vTXDMLaVeHVg7w==")
