8a9f081fd8b5ba8824e99baee6057750fc708a9b
dingdayu <614422099@qq.com>
2020-06-11 20:20:05 +0800

    Add pkg/json
    Signed-off-by: dingdayu <614422099@qq.com>

---
+++ pkg/json/jsoniter.go
@@ -0,0 +1,25 @@
+package json
+
+import (
+	jsoniter "github.com/json-iterator/go"
+)
+
+var (
+	json = jsoniter.ConfigCompatibleWithStandardLibrary
+	// Marshal is exported by gin/json package.
+	Marshal         = json.Marshal
+	MarshalToString = jsoniter.MarshalToString
+
+	// Unmarshal is exported by gin/json package.
+	Unmarshal = json.Unmarshal
+
+	// UnmarshalFromString is exported by module/json package.
+	UnmarshalFromString = json.UnmarshalFromString
+
+	// MarshalIndent is exported by gin/json package.
+	MarshalIndent = json.MarshalIndent
+	// NewDecoder is exported by gin/json package.
+	NewDecoder = json.NewDecoder
+	// NewEncoder is exported by gin/json package.
+	NewEncoder = json.NewEncoder
+)
