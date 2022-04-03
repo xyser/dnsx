06d884a058de1e307cae799172a8856465609b36
dingdayu <614422099@qq.com>
2022-04-03 15:43:19 +0800

    support multi-question in aaaa
    Signed-off-by: dingdayu <614422099@qq.com>

--- api/controller/dns/aaaa.go
+++ api/controller/dns/aaaa.go
@@ -10,16 +10,18 @@ import (
 
 // TypeAAAA query aaaa
 func TypeAAAA(msg *dns.Msg) error {
-	domain := msg.Question[0].Name
-	rrs, err := dao.GetRecordByNameAndType(domain, msg.Question[0].Qtype)
-	if err != nil {
-		return err
+	for _, question := range msg.Question {
+		rrs, err := dao.GetRecordByNameAndType(question.Name, question.Qtype)
+		if err != nil {
+			return err
+		}
+		for _, rr := range rrs {
+			msg.Answer = append(msg.Answer, &dns.AAAA{
+				Hdr:  dns.RR_Header{Name: question.Name, Rrtype: question.Qtype, Class: dns.ClassINET, Ttl: rr.TTL},
+				AAAA: net.ParseIP(rr.Value),
+			})
+		}
 	}
-	for _, rr := range rrs {
-		msg.Answer = append(msg.Answer, &dns.AAAA{
-			Hdr:  dns.RR_Header{Name: msg.Question[0].Name, Rrtype: msg.Question[0].Qtype, Class: dns.ClassINET, Ttl: rr.TTL},
-			AAAA: net.ParseIP(rr.Value),
-		})
-	}
+
 	return nil
 }
